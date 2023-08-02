ab491928e5a380107f558e786344fe27c6f8bb13
Axelbresa <bresanovichaxel43@gmail.com>
2023-08-02 14:52:56 -0300

    se implemento las variables de entorno

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ const app = express();
 // Middlewares
 // TODO: Implementar middlewares
 
+
+const PORT = process.env.PORT || 3200;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use(require('./src/routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
-
+process.env.PORT 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, function (req, res) {
+    console.log("la app esta escuchando en http://localhost: " + PORT);
+  });
